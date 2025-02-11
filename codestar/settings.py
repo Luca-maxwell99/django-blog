dcdd86e1666a5bce37b392c4eb0722a069c40d37
Luca Maxwell <lucamaxwelldev@gmail.com>
2025-02-11 15:47:51 +0000

    add environment vars

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l-lb^7#dah3k+(jtg!v0*5puw1-7lj0!zhoh^5m)=yg$1cms68"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
